9b9c89d76aae0f845347cdd1793be8e226825653
emmanouil emmanouil <emmanouil.e25@gmail.com>
2024-02-17 04:12:50 +0000

    1) fix bug with input component to display default value

--- app/components/Form/Input/Input.tsx
+++ app/components/Form/Input/Input.tsx
@@ -18,13 +18,15 @@ type Props = MyInputProps & Omit<InputHTMLAttributes<HTMLInputElement>, "name">;
 const Input = ({ name, label, type, className, ...rest }: Props) => {
   const { error, getInputProps } = useField(name || "input");
   const mergedClasses = twMerge(`w-full ${className}`);
+  const { defaultValue, ...restInputProps } = getInputProps({
+    id: name,
+    type,
+    ...rest,
+  });
   return (
-    <TextField className={className}>
+    <TextField defaultValue={defaultValue} className={className}>
       {label && <Label htmlFor={name}>{label}</Label>}
-      <ReactAriaInput
-        {...getInputProps({ id: name, type, ...rest })}
-        className={mergedClasses}
-      />
+      <ReactAriaInput {...restInputProps} className={mergedClasses} />
       {error && <span className="text-red-600 text-base">{error}</span>}
     </TextField>
   );
